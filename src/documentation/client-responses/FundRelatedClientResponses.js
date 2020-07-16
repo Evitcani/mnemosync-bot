f1e81caad4ce952a1e22c18282373f17b6af6078
Evitcani <33763283+evitcani@users.noreply.github.com>
2020-07-16 16:25:18 -0500

    Better formatting.

--- src/documentation/client-responses/FundRelatedClientResponses.js
+++ src/documentation/client-responses/FundRelatedClientResponses.js
@@ -9,14 +9,14 @@ const discord_js_1 = require("discord.js");
  */
 class FundRelatedClientResponses {
     static NOT_ENOUGH_MONEY(currentMoney, amtToWithdraw, difference) {
-        let input1 = StringUtility_1.StringUtility.numberWithCommas(currentMoney);
-        let input2 = StringUtility_1.StringUtility.numberWithCommas(amtToWithdraw);
-        let input3 = StringUtility_1.StringUtility.numberWithCommas(difference);
+        let input1 = StringUtility_1.StringUtility.numberWithCommas(Math.abs(currentMoney));
+        let input2 = StringUtility_1.StringUtility.numberWithCommas(Math.abs(amtToWithdraw));
+        let input3 = StringUtility_1.StringUtility.numberWithCommas(Math.abs(difference));
         return this.getBasicEmbed()
             .setTitle('Too little, too late!')
-            .addField('Remaining funds', input1, true)
-            .addField('Attempted to withdraw', input2, true)
-            .addField(null, `You don't have enough money to do that. You are short ${input3} gp!`, true);
+            .addField('Remaining funds', `${input1} gp`, true)
+            .addField('Attempted to withdraw', `${input2} gp`, true)
+            .addField("Description", `You don't have enough money to do that. You are short ${input3} gp!`, false);
     }
     /**
      * Gets the
