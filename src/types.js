b5091a5318925e5968e15d89f1f7bcf834afc2e6
Evitcani <33763283+evitcani@users.noreply.github.com>
2020-07-15 19:51:46 -0500

    Default bot config.

---
+++ src/types.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TYPES = void 0;
+exports.TYPES = {
+    Bot: Symbol("Bot"),
+    Client: Symbol("Client"),
+    Token: Symbol("Token"),
+    MessageResponder: Symbol("MessageResponder"),
+    PingFinder: Symbol("PingFinder")
+};
+//# sourceMappingURL=types.js.map
