a5bb3fc917e51aaa8d77a8c50b59f40bdfc9bafd
Evitcani <33763283+evitcani@users.noreply.github.com>
2020-07-16 01:26:16 -0500

    More logging to track down bug.

--- src/utilities/MoneyUtility.js
+++ src/utilities/MoneyUtility.js
@@ -120,22 +120,22 @@ class MoneyUtility {
     static searchForMoneyType(arg) {
         let place = arg.search("g");
         if (place >= 0) {
-            const num = arg.substr(0, place + 1);
+            const num = arg.substr(0, place);
             return num + " gold";
         }
         place = arg.search("c");
         if (place >= 0) {
-            const num = arg.substr(0, place + 1);
+            const num = arg.substr(0, place);
             return num + " copper";
         }
         place = arg.search("s");
         if (place >= 0) {
-            const num = arg.substr(0, place + 1);
+            const num = arg.substr(0, place);
             return num + " silver";
         }
         place = arg.search("p");
         if (place >= 0) {
-            const num = arg.substr(0, place + 1);
+            const num = arg.substr(0, place);
             return num + " platinum";
         }
         return arg;
