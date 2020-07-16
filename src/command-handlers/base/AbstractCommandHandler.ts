dd91ba1c66604d8f2270ea006c75bc1a9ab8b61a
Evitcani <33763283+evitcani@users.noreply.github.com>
2020-07-16 15:58:01 -0500

    Add injectable to abstract.

--- src/command-handlers/base/AbstractCommandHandler.ts
+++ src/command-handlers/base/AbstractCommandHandler.ts
@@ -1,6 +1,8 @@
 import {Message} from "discord.js";
 import {Command} from "../../models/generic/Command";
+import {injectable} from "inversify";
 
+@injectable()
 export abstract class AbstractCommandHandler {
     abstract async handleCommand (command: Command, message: Message): Promise<Message | Message[]>;
 }
