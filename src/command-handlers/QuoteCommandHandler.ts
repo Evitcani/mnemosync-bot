093c9c16a442de6d2dcb861aaf5de2473556c067
Evitcani <33763283+evitcani@users.noreply.github.com>
2020-07-18 14:55:46 -0500

    Complete quote command.

--- src/command-handlers/QuoteCommandHandler.ts
+++ src/command-handlers/QuoteCommandHandler.ts
@@ -1,7 +1,12 @@
-import {injectable} from "inversify";
+import {inject, injectable} from "inversify";
 import {AbstractCommandHandler} from "./base/AbstractCommandHandler";
 import {Command} from "../models/generic/Command";
-import {Message} from "discord.js";
+import {Client, Collection, Message, Snowflake, TextChannel} from "discord.js";
+import {TYPES} from "../types";
+import {SpecialChannelService} from "../database/SpecialChannelService";
+import {SpecialChannelDesignation} from "../enums/SpecialChannelDesignation";
+import {QuoteRelatedClientResponses} from "../documentation/client-responses/QuoteRelatedClientResponses";
+const delay = require('delay');
 
 /**
  * Handles the "quote" command from users. This command allows a user to designate a channel as the "quote" channel and
@@ -9,8 +14,78 @@ import {Message} from "discord.js";
  */
 @injectable()
 export class QuoteCommandHandler extends AbstractCommandHandler {
-    async handleCommand(command: Command, message: Message): Promise<Message | Message[]> {
-        return undefined;
+    private client: Client;
+    private specialChannelService: SpecialChannelService;
+
+    constructor(@inject(TYPES.Client) client: Client,
+                @inject(TYPES.SpecialChannelService) specialChannelService: SpecialChannelService,) {
+        super();
+        this.client = client;
+        this.specialChannelService = specialChannelService;
     }
 
+    async handleCommand(command: Command, message: Message): Promise<Message | Message[]> {
+        // Registering the channel.
+        if (command.getInput() != null && command.getInput().toLowerCase() == "here") {
+            return this.registerQuoteChannel(message);
+        }
+
+        // Otherwise gets a random quote from the quote channel.
+        return this.getRandomQuote(message).then((msg) => {
+            return message.channel.send(QuoteRelatedClientResponses.QUOTED_MESSAGE(msg));
+        });
+    }
+
+    private async registerQuoteChannel (message: Message): Promise<Message | Message[]> {
+        return this.specialChannelService.addSpecialChannel(message.guild.id, SpecialChannelDesignation.QUOTE_CHANNEL, message.channel.id)
+            .then(() => {
+                return message.channel.send("Registered this channel as your quotes channel!");
+            });
+    }
+
+    private async getRandomQuote (message: Message): Promise<Message> {
+        return this.getQuoteChannel(message).then((channelId) => {
+            return this.getAllMessages(message, channelId).then((messages) => {
+                return messages.random();
+            });
+        });
+    }
+
+    private async getQuoteChannel(message: Message): Promise<string> {
+        return this.specialChannelService.getSpecialChannel(message.guild.id, SpecialChannelDesignation.QUOTE_CHANNEL)
+            .then((channel) => {
+                return channel.channel_id;
+            });
+    }
+
+    private async getAllMessages(message: Message, channelId: string): Promise<Collection<Snowflake, Message>> {
+        // @ts-ignore
+        const channel: TextChannel = message.guild.channels.resolve(channelId);
+
+        if (channel.messages.cache.size > 0) {
+            return channel.messages.cache;
+        }
+
+        return this.fetchAllMessages(channel, null);
+    }
+
+    private async fetchAllMessages(channel: TextChannel, beforeMessageId: Snowflake | null): Promise<Collection<Snowflake, Message>> {
+        // Fetch the channels.
+        return channel.messages.fetch({limit: 100, before: beforeMessageId}).then(async (messages) => {
+            if (messages.size < 100) {
+                return messages;
+            }
+
+            await delay(250);
+
+            const oldestMsg = messages.first().id;
+
+            // Go until we get all the messages.
+            return this.fetchAllMessages(channel, oldestMsg).then((returnedMessages) => {
+                const concatMessages = messages.concat(returnedMessages);
+                channel.messages.cache = concatMessages;
+                return concatMessages;
+            });
+        })
+    }
 }
---
+++ src/documentation/client-responses/QuoteRelatedClientResponses.ts
@@ -0,0 +1,24 @@
+import {Message, MessageEmbed} from "discord.js";
+import {BasicEmbed} from "../BasicEmbed";
+
+/**
+ * The responses related to the quotes command.
+ */
+export class QuoteRelatedClientResponses {
+    static QUOTED_MESSAGE (message: Message): MessageEmbed {
+        const msg = BasicEmbed.get()
+            .setAuthor(message.author, message.author.avatarURL(), message.url)
+            .setTitle("A quote from the past...")
+            .setDescription(message.content)
+            .setTimestamp(message.createdAt);
+        const attachments = message.attachments;
+        if (attachments != null && attachments.size > 0) {
+            attachments.forEach((attachment) => {
+                // @ts-ignore TS isn't picking up the type correctly.
+                msg.attachFiles(attachment);
+            })
+        }
+
+        return msg;
+    }
+}
