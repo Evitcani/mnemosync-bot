fed7872b7c2bfb1f99676d3f2ceb935494999ad4
Evitcani <33763283+evitcani@users.noreply.github.com>
2020-07-18 15:13:58 -0500

    Change footer.

--- src/documentation/client-responses/QuoteRelatedClientResponses.js
+++ src/documentation/client-responses/QuoteRelatedClientResponses.js
@@ -9,9 +9,9 @@ class QuoteRelatedClientResponses {
     static QUOTED_MESSAGE(message) {
         const msg = BasicEmbed_1.BasicEmbed.get()
             .setAuthor(message.author.username, message.author.avatarURL(), message.url)
-            .setTitle("A quote from the past...")
             .setDescription(message.content)
-            .setTimestamp(message.createdAt);
+            .setTimestamp(message.createdAt)
+            .setFooter("A quote from the past...");
         const attachments = message.attachments;
         if (attachments != null && attachments.size > 0) {
             attachments.forEach((attachment) => {
