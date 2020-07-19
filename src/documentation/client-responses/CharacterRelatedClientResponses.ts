fca8a006f955a3d3ba347164eae6ed47f921d716
Evitcani <33763283+evitcani@users.noreply.github.com>
2020-07-19 17:09:19 -0500

    Fix join command and change prefix for subcommands.

--- src/documentation/client-responses/CharacterRelatedClientResponses.ts
+++ src/documentation/client-responses/CharacterRelatedClientResponses.ts
@@ -3,6 +3,7 @@ import {Character} from "../../models/database/Character";
 import {BasicEmbed} from "../BasicEmbed";
 import {Bot} from "../../bot";
 import {Commands} from "../commands/Commands";
+import {Subcommands} from "../commands/Subcommands";
 
 export class CharacterRelatedClientResponses {
     static NOW_PLAYING_AS_CHARACTER(character: Character, newlyCreated: boolean): MessageEmbed {
@@ -13,7 +14,7 @@ export class CharacterRelatedClientResponses {
         }
 
         embed.setDescription(`You are now playing as ${newlyCreated ? "newly created character " : ""}${character.name}. ` +
-            `To switch characters, type \`${Bot.PREFIX}${Commands.CHARACTER} --switch ` +
+            `To switch characters, type \`${Bot.PREFIX}${Commands.CHARACTER} ${Bot.PREFIX_SUBCOMMAND}${Subcommands.SWITCH} ` +
             `[character name]\`. To view all of your characters, type ` +
             `\`${Bot.PREFIX}${Commands.WHICH} ${Commands.CHARACTER}\`.`);
 
