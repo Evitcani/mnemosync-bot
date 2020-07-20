87295c31cb7740d0ba47c8de03dd782cae39510a
Evitcani <33763283+evitcani@users.noreply.github.com>
2020-07-20 12:00:10 -0500

    Established connection so it may be used elsewhere.

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,22 @@ require('dotenv').config(); // Recommended way of loading dotenv
 import container from "./inversify.config";
 import {TYPES} from "./types";
 import {Bot} from "./bot";
+import {createConnection} from "typeorm";
 
 let bot = container.get<Bot>(TYPES.Bot);
 
-bot.listen().then(() => {
-    console.log('Logged in!')
-}).catch((error) => {
-    console.log('Oh no! ', error)
+createConnection({
+    type: "postgres",
+    url: this.databaseUrl,
+    entities: [
+        __dirname + "/entity/*.js"
+    ],
+    synchronize: true,
+}).then(() => {
+    bot.listen().then(() => {
+        console.log('Logged in!')
+    }).catch((error) => {
+        console.log('Oh no! ', error)
+    });
 });
+
