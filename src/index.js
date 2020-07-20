c6675fd58e9464288a84663ef2607086a844cc74
Evitcani <33763283+evitcani@users.noreply.github.com>
2020-07-20 12:00:10 -0500

    Established connection so it may be used elsewhere.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ require("reflect-metadata");
 require('dotenv').config(); // Recommended way of loading dotenv
 const inversify_config_1 = require("./inversify.config");
 const types_1 = require("./types");
+const typeorm_1 = require("typeorm");
 let bot = inversify_config_1.default.get(types_1.TYPES.Bot);
-bot.listen().then(() => {
-    console.log('Logged in!');
-}).catch((error) => {
-    console.log('Oh no! ', error);
+typeorm_1.createConnection({
+    type: "postgres",
+    url: this.databaseUrl,
+    entities: [
+        __dirname + "/entity/*.js"
+    ],
+    synchronize: true,
+}).then(() => {
+    bot.listen().then(() => {
+        console.log('Logged in!');
+    }).catch((error) => {
+        console.log('Oh no! ', error);
+    });
 });
 //# sourceMappingURL=index.js.map
