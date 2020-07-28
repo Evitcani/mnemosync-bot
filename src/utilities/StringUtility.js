de3db6c6a754ebd1703c8ab0dbe0a6f3d87e05bd
Evitcani <33763283+evitcani@users.noreply.github.com>
2020-07-28 15:19:30 -0500

    More logging.

--- src/utilities/StringUtility.js
+++ src/utilities/StringUtility.js
@@ -28,8 +28,11 @@ class StringUtility {
         if (input == null) {
             return null;
         }
+        console.log("First: " + input);
         let correctedInput = input.replace(this.fancyQuote1, "'");
+        console.log("Second: " + correctedInput);
         correctedInput = correctedInput.replace(this.fancyQuote2, "\"");
+        console.log("Third: " + correctedInput);
         return correctedInput;
     }
     /**
--- src/utilities/StringUtility.ts
+++ src/utilities/StringUtility.ts
@@ -52,8 +52,11 @@ export class StringUtility {
         if (input == null) {
             return null;
         }
+        console.log("First: " + input);
         let correctedInput = input.replace(this.fancyQuote1, "'");
+        console.log("Second: " + correctedInput);
         correctedInput = correctedInput.replace(this.fancyQuote2, "\"");
+        console.log("Third: " + correctedInput);
         return correctedInput;
     }
 
